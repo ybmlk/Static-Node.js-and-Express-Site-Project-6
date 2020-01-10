aec8762e100f3b5fb504b176479ac92a32602c89
Yonatan Melka <ybmelka2@gmail.com>
2020-01-09 21:09:51 -0600

    Complete all project pages

--- routes/project.js
+++ routes/project.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { projects } = require('../data/data.json');
 
-router.get('/', (req, res) => {
-    res.render('project');
-})
+// '/projects' will redirect to the first project
+router.get('/', (req, res)=> {
+    res.redirect('/project/1')
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const currentProject = projects[parseInt(id) - 1]
+
+    const { project_name } = currentProject;
+    const { description } = currentProject;
+    const { technologies } = currentProject;
+    const { live_link } = currentProject;
+    const { github_link } = currentProject;
+    const { image_urls } = currentProject;
+
+    res.render('project', { project_name, description, technologies, live_link, github_link, image_urls });
+});
 
 module.exports = router;
